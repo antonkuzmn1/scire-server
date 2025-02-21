38ea6c3583e8e2022675ec994cce65a6a136188f
anton <antonkuzmn1@gmail.com>
2025-02-21 13:05:06 +0700

    clear

--- src/tools/router.ts
+++
@@ -1,8 +0,0 @@
-import {Router} from 'express';
-
-// /api
-export const router = Router();
-
-// router.use('/security', securityController);
-// router.use('/table', tableController);
-// router.use('/log', logController);
