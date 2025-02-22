07ccd0218695cbd7f79e030f6158e26ca0753efc
anton <antonkuzmn1@gmail.com>
2025-02-22 18:49:47 +0700

    added services

--- app/dependencies/services.py
+++ app/dependencies/services.py
@@ -1,14 +1,15 @@
-# from sqlalchemy.ext.asyncio import AsyncSession
-# from fastapi import Depends
-#
-# from app.db import get_db
-# from app.repositories.file_repo import FileRepository
-# from app.services.file_service import FileService
-# from app.settings import settings
-#
-#
-# async def get_file_service(db: AsyncSession = Depends(get_db)) -> FileService:
-#     return FileService(
-#         repository=FileRepository(db),
-#         upload_dir=settings.UPLOAD_DIR
-#     )
+from sqlalchemy.ext.asyncio import AsyncSession
+from fastapi import Depends
+
+from app.db import get_db
+from app.services.message_service import MessageService
+
+from app.services.ticket_service import TicketService
+
+
+async def get_ticket_service(db: AsyncSession = Depends(get_db)) -> TicketService:
+    return TicketService(db=db)
+
+
+async def get_message_service(db: AsyncSession = Depends(get_db)) -> MessageService:
+    return MessageService(db=db)
